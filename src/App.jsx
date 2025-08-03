b9331b069b73b6c0810a10ad6eef4920ffe629fa
Shivam Keshari <shivamkeshari990@gmail.com>
2025-08-03 20:09:49 +0530

    Second

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Hello from './Hello.jsx'
-import Random from './Random.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Hello from "./Hello.jsx";
+import Random from "./Random.jsx";
 
 function App() {
   return (
     <>
-       <h1>This is First react App</h1>
-       <Hello></Hello>
-       <Random></Random>
-    </>
-  )
-}
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">This is First React App</h5>
 
-export default App
+          <Hello />
+          <Random />
+
+          <p className="card-text">
+            Some additional description or text can go here if needed.
+          </p>
+
+          <a href="#" className="btn btn-primary">
+            Go
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
